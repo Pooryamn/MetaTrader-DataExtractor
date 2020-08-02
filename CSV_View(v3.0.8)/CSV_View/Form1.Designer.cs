ae35d4ff9972d1bca13dd76dab20e432e7a77e46
MrPoorya <poorya.m.n.b@gmail.com>
2020-08-02 21:17:56 +0430

    this is a C# source code.this application runs on client side and execute clienct.py every one second

---
+++ CSV_View(v3.0.8)/CSV_View/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace CSV_View
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rowCountToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutCSVViewerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.webSiteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.supportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_Search = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1730, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.rowCountToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // rowCountToolStripMenuItem
+            // 
+            this.rowCountToolStripMenuItem.Name = "rowCountToolStripMenuItem";
+            this.rowCountToolStripMenuItem.Size = new System.Drawing.Size(150, 24);
+            this.rowCountToolStripMenuItem.Text = "Row Count";
+            this.rowCountToolStripMenuItem.Click += new System.EventHandler(this.rowCountToolStripMenuItem_Click_1);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(150, 24);
+            this.saveAsToolStripMenuItem.Text = "Save as ...";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(150, 24);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem1
+            // 
+            this.helpToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutCSVViewerToolStripMenuItem,
+            this.webSiteToolStripMenuItem,
+            this.supportToolStripMenuItem});
+            this.helpToolStripMenuItem1.Name = "helpToolStripMenuItem1";
+            this.helpToolStripMenuItem1.Size = new System.Drawing.Size(53, 24);
+            this.helpToolStripMenuItem1.Text = "Help";
+            this.helpToolStripMenuItem1.Click += new System.EventHandler(this.helpToolStripMenuItem1_Click);
+            // 
+            // aboutCSVViewerToolStripMenuItem
+            // 
+            this.aboutCSVViewerToolStripMenuItem.Name = "aboutCSVViewerToolStripMenuItem";
+            this.aboutCSVViewerToolStripMenuItem.Size = new System.Drawing.Size(200, 24);
+            this.aboutCSVViewerToolStripMenuItem.Text = "About CSV_Viewer";
+            this.aboutCSVViewerToolStripMenuItem.Click += new System.EventHandler(this.aboutCSVViewerToolStripMenuItem_Click);
+            // 
+            // webSiteToolStripMenuItem
+            // 
+            this.webSiteToolStripMenuItem.Name = "webSiteToolStripMenuItem";
+            this.webSiteToolStripMenuItem.Size = new System.Drawing.Size(200, 24);
+            this.webSiteToolStripMenuItem.Text = "WebSite";
+            this.webSiteToolStripMenuItem.Click += new System.EventHandler(this.webSiteToolStripMenuItem_Click);
+            // 
+            // supportToolStripMenuItem
+            // 
+            this.supportToolStripMenuItem.Name = "supportToolStripMenuItem";
+            this.supportToolStripMenuItem.Size = new System.Drawing.Size(200, 24);
+            this.supportToolStripMenuItem.Text = "Support";
+            this.supportToolStripMenuItem.Click += new System.EventHandler(this.supportToolStripMenuItem_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("B Zar", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.AppWorkspace;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HotTrack;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.Desktop;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 99);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1524, 500);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.dataGridView1_Scroll);
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            this.dataGridView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dataGridView1_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Search :";
+            // 
+            // txt_Search
+            // 
+            this.txt_Search.Location = new System.Drawing.Point(79, 57);
+            this.txt_Search.Name = "txt_Search";
+            this.txt_Search.Size = new System.Drawing.Size(239, 22);
+            this.txt_Search.TabIndex = 3;
+            this.txt_Search.TextChanged += new System.EventHandler(this.txt_Search_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1730, 553);
+            this.Controls.Add(this.txt_Search);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "CSV Viewer";
+            this.MaximumSizeChanged += new System.EventHandler(this.Form1_MaximumSizeChanged);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem aboutCSVViewerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem webSiteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem supportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rowCountToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_Search;
+
+    }
+}
+
---
+++ CSV_View(v3.0.8)/CSV_View/Form1.cs
@@ -0,0 +1,344 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CSV_View
+{
+    public partial class Form1 : Form
+    {
+
+        private int oldRowIndex;
+        DataTable dt;
+        string path;
+        
+
+        public Form1()
+        {
+            // RUN CLINET SILDE PROGRAM :
+            path = AppDomain.CurrentDomain.BaseDirectory;
+            path += "\\Data\\Client.exe";
+            Process.Start(path);
+
+            InitializeComponent();
+            oldRowIndex = 0;
+            BindCsv();
+            timer1.Start();
+        }
+
+        private void BindCsv()
+        {
+
+
+            //DataTable dt = new DataTable();
+            dt = new DataTable();
+
+            string[] lines = System.IO.File.ReadAllLines("Data/DATA_FILE.csv");
+            if (lines.Length > 0)
+            {
+                if (lines.Length < oldRowIndex)
+                {
+                    oldRowIndex = 0;
+                }
+                string[] HeaderLabels;
+                HeaderLabels = new String[] {"ردیف","Name","تاریخ","ساعت","عرضه","تقاضا","آخرین قیمت","بیشترین قیمت","کمترین قیمت","تغییر روزانه","تعداد معاملات","حجم معاملات","قیمت باز","قیمت بسته"};
+                foreach (string headerWord in HeaderLabels)
+                {
+                    dt.Columns.Add(new DataColumn(headerWord));
+                }
+
+                // DATA :
+
+                System.IO.StreamReader Symbol = new System.IO.StreamReader("Data/Symbols.txt");
+
+                for (int r = 1; r < lines.Length; r++)
+                {
+
+                    string[] dataWords = lines[r].Split(',');
+
+                    DataRow dr = dt.NewRow();
+
+                    // Data :
+                    String Date = dataWords[0];
+                    dr[2] = Date;
+
+                    String Time = dataWords[1];
+                    dr[3] = Time;
+
+                    String BID = dataWords[2].Split('.')[0];
+                    dr[4] = BID;
+
+                    String ASK = dataWords[3].Split('.')[0];
+                    dr[5] = ASK;
+
+                    String Last = dataWords[4].Split('.')[0];
+                    dr[6] = Last;
+
+                    String Last_H = dataWords[5].Split('.')[0];
+                    dr[7] = Last_H;
+
+                    String Last_L = dataWords[6].Split('.')[0];
+                    dr[8] = Last_L;
+
+                    String Change = dataWords[7];
+                    if (Change.Length >= 5)
+                    {
+                        Change = Change.Remove(5);
+                        Change += " %";
+                    }
+                    dr[9] = Change;
+
+                    String Deals = dataWords[8].Split('.')[0];
+                    dr[10] = Deals;
+
+                    String Deals_V = dataWords[9].Split('.')[0];
+                    dr[11] = Deals_V;
+
+                    String Opens = dataWords[10].Split('.')[0];
+                    dr[12] = Opens;
+
+                    String Closes = dataWords[11].Split('.')[0];
+                    dr[13] = Closes;
+
+                    string SymName;
+                    SymName = Symbol.ReadLine();
+                    dr[1] = SymName;
+                    dr[0] = r.ToString();
+
+                    dt.Rows.Add(dr);
+
+                }
+                Symbol.Close();
+
+            }
+
+
+            if (txt_Search.Text.Length != 0)
+            {
+                show_search();
+            }
+            else
+            {
+
+                if (dt.Rows.Count > 0)
+                {
+
+                    dataGridView1.DataSource = dt;
+                    dataGridView1.Columns[4].DefaultCellStyle.ForeColor = Color.Blue;
+                    dataGridView1.Columns[5].DefaultCellStyle.ForeColor = Color.Red;
+                    dataGridView1.Columns[0].DefaultCellStyle.BackColor = Color.FromArgb(255, 128, 192);
+                    dataGridView1.Columns[1].DefaultCellStyle.BackColor = Color.FromArgb(145, 145, 255);
+                    dataGridView1.Columns[6].DefaultCellStyle.BackColor = Color.FromArgb(181, 230, 29);
+                    //dataGridView1.Rows[oldRowIndex].Selected = true;
+                    //dataGridView1.FirstDisplayedScrollingRowIndex = dataGridView1.SelectedRows[oldRowIndex].Index;
+                    //if (oldRowIndex >= 11)
+                    //{
+                        dataGridView1.FirstDisplayedScrollingRowIndex = oldRowIndex;
+                    //}
+                    //else
+                    //{
+                        //dataGridView1.FirstDisplayedScrollingRowIndex = 0;
+                    //}
+                }
+            }
+            
+            try
+            {
+                Process.Start(path);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            BindCsv();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            oldRowIndex = e.RowIndex;
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            //dataGridView1.ClearSelection();
+        }
+
+        private void txt_IP_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btn_ok_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("POORYA MOHAMMADI","ALERT");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void helpToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // open file dialog
+            // save dt data to a csv
+            SaveFileDialog SaveFile = new SaveFileDialog();
+            SaveFile.Filter = "CSV (*.csv) | *.csv";
+            SaveFile.FileName = "Output.csv";
+
+            bool fileErr = false;
+
+            if (SaveFile.ShowDialog() == DialogResult.OK)
+            {
+                if (File.Exists(SaveFile.FileName))
+                {
+                    try
+                    {
+                        File.Delete(SaveFile.FileName);
+                    }
+                    catch(IOException ex)
+                    {
+                        fileErr = true;
+                        MessageBox.Show("Can't write data to file" + ex.Message);
+                    }
+                }
+                if (!fileErr)
+                {
+                    try
+                    {
+                        int columnCount = dataGridView1.Columns.Count;
+                        string ColNames = "";
+                        string[] outputCSV = new string[dataGridView1.Rows.Count + 1];
+
+                        for (int i = 0; i < columnCount; i++)
+                        {
+                            ColNames += dataGridView1.Columns[i].HeaderText.ToString() + ",";
+                        }
+
+                        outputCSV[0] += ColNames;
+
+                        for (int i = 1; (i - 1) < dataGridView1.Rows.Count; i++)
+                        {
+                            for (int j = 0; j < columnCount; j++)
+                            {
+                                outputCSV[i] += dataGridView1.Rows[i - 1].Cells[j].Value.ToString() + ",";
+                            }
+                        }
+                        File.WriteAllLines(SaveFile.FileName, outputCSV, Encoding.UTF8); // very important part
+                        MessageBox.Show("Data exported !!", "Info");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error : " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void aboutCSVViewerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("CSV Viewer Version 1.0.6\n\nPoorya Mohammadi", "About software");
+        }
+
+        private void webSiteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.github.com/Pooryamn");
+        }
+
+        private void supportToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Support info", "Support");
+        }
+
+        private void rowCountToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int rowCount = dataGridView1.Rows.Count;
+            MessageBox.Show("Row Count :" + rowCount.ToString(), "Rows");
+        }
+
+        private void Form1_MaximumSizeChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //dataGridView1.Width = this.Width;
+            //dataGridView1.Height = this.Height;
+            dataGridView1.Size = new Size(this.Width, this.Height);
+        }
+
+        private void rowCountToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            int rowCount = dataGridView1.Rows.Count;
+            MessageBox.Show("Row Count :" + rowCount.ToString(), "Rows");
+        }
+
+        private void txt_Search_TextChanged(object sender, EventArgs e)
+        {
+            show_search();   
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            System.IO.File.Copy("RawData/DATA_FILE.csv", "Data/DATA_FILE.csv", true);
+            System.IO.File.Copy("RawData/Symbols.txt", "Data/Symbols.txt", true);
+        }
+
+        private void show_search()
+        {
+            DataView dv = new DataView(dt);
+            dv.RowFilter = string.Format("Name LIKE '%{0}%'", txt_Search.Text);
+            dataGridView1.DataSource = dv;
+        }
+
+        private void dataGridView1_Scroll(object sender, ScrollEventArgs e)
+        {
+            //if (e.NewValue == 0)
+            //{
+                //e.NewValue = e.OldValue;
+            //}
+            oldRowIndex = e.NewValue;
+
+        }
+
+        private void dataGridView1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //oldRowIndex++;
+            //if (oldRowIndex > 100)
+            //{
+                //oldRowIndex = 100;
+           // }
+            //dataGridView1.Rows[oldRowIndex].Selected = true;
+        }
+
+    } 
+
+}
